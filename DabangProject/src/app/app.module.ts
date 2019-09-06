0feb5a29cfb0e3fb520f84f0c5c23c92331af407
teddy <teddykim9298@gmail.com>
2019-09-06 15:39:18 +1000

    Organizing Components with downloaded Template html

--- DabangProject/src/app/app.module.ts
+++ DabangProject/src/app/app.module.ts
@@ -3,14 +3,31 @@ import { NgModule } from '@angular/core';
 
 import { AppRoutingModule } from './app-routing.module';
 import { AppComponent } from './app.component';
+import { FooterComponent } from './footer/footer.component';
+import { RoutingModule } from './routing/routing.module';
+import { NavigationComponent } from './navigation/navigation.component';
+import { CarouselComponent } from './carousel/carousel.component';
+import { RestaurantComponent } from './restaurant/restaurant.component';
+import { MenuComponent } from './menu/menu.component';
+import { ReservationComponent } from './reservation/reservation.component';
+import { LocationComponent } from './location/location.component';
+
 
 @NgModule({
   declarations: [
-    AppComponent
+    AppComponent,
+    FooterComponent,
+    NavigationComponent,
+    CarouselComponent,
+    RestaurantComponent,
+    MenuComponent,
+    ReservationComponent,
+    LocationComponent
   ],
   imports: [
     BrowserModule,
-    AppRoutingModule
+    AppRoutingModule,
+    RoutingModule
   ],
   providers: [],
   bootstrap: [AppComponent]
